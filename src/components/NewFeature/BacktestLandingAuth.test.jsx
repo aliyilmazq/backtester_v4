ccf785d06b04c830220c4ae738e816779e34c0d2
Ali YILMAZ <831590@gmail.com>
2025-07-26 00:58:25 +0300

    commit

--- src/components/NewFeature/BacktestLandingAuth.test.jsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-
-import BacktestLandingAuth from './BacktestLandingAuth';
-
-test('renders learn react link', () => {
-  render(<BacktestLandingAuth />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
